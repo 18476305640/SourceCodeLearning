c92ecbdcc04f6e0d7d8bbfa5e347fd8984d6ced1
zhuangjie <2119299531@qq.com>
2022-05-01 00:10:32 +0800

    'spring-ioc'

---
+++ src/com/zjazn/spring/_1_spring入门案例/User.java
@@ -0,0 +1,7 @@
+package com.zjazn.spring._1_spring入门案例;
+
+public class User {
+    public void run() {
+        System.out.println("不错哟~");
+    }
+}
---
+++ src/com/zjazn/spring/_1_spring入门案例/main.java
@@ -0,0 +1,17 @@
+package com.zjazn.spring._1_spring入门案例;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class main {
+    public static void main(String[] args) {
+        //加载spring配置文件
+        BeanFactory context = new ClassPathXmlApplicationContext("_1_spring入门案例.xml");
+        //从中获取生成的对象
+        User user = context.getBean("user", User.class);
+        //对象的调用测试
+        user.run();
+    }
+}
+
