fc6458267565d465312d790693e95fd690a5c43c
zhuangjie <2119299531@qq.com>
2022-05-01 00:16:36 +0800

    '移动了一些文件'

--- src/com/scl/spring/_1_spring入门案例/main.java
+++ src/com/scl/spring/_1_spring入门案例/main.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class main {
     public static void main(String[] args) {
         //加载spring配置文件
-        BeanFactory context = new ClassPathXmlApplicationContext("_1_spring入门案例.xml");
+        BeanFactory context = new ClassPathXmlApplicationContext("com/scl/spring/_1_spring入门案例/_1_spring入门案例.xml");
         //从中获取生成的对象
         User user = context.getBean("user", User.class);
         //对象的调用测试
